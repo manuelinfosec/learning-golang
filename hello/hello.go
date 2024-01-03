807bcd3d509f70245159934e6490384c412f70c0
Manuel <manuelinfosec@gmail.com>
2024-01-03 22:33:55 +0100

    Golang: Return and handle an error

--- hello/hello.go
+++ hello/hello.go
@@ -1,12 +1,27 @@
 package main
 
 import (
-	"example.com/greetings"
 	"fmt"
+	"log"
+
+	"example.com/greetings"
 )
 
 func main() {
-	// Get a greeting message and print it
-	message := greetings.Hello("Gladys")
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message
+	message, err := greetings.Hello("")
+	// exit the program
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console
 	fmt.Println(message)
 }
