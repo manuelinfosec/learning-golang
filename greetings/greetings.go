73b107b2f2ba270b314fab966c3830307b85fc2a
Manuel <manuelinfosec@gmail.com>
2024-01-03 22:24:47 +0100

    Greetings: Returning errors

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,9 +1,17 @@
 package greetings
 
 import "fmt"
+import "errors"
 
-func Hello(name string) string {
-	// Return a greeting that embeds the name in a message.
+// Hello returns a greeting for the named person
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with an message
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// If a name was received, return a value that embeds the name
+	// in a greeting message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
