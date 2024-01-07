17f421a4c9a1c268c7509cf72fecac21a30c9e38
Manuel <manuelinfosec@gmail.com>
2024-01-07 20:24:39 +0100

    feat(wc): counting by bytes

--- wc/main.go
+++ wc/main.go
@@ -13,13 +13,18 @@ import (
 	"os"
 )
 
-func count(r io.Reader, countLines bool) int {
+func count(r io.Reader, countLines bool, countBytes bool) int {
 	// A scanner is used to read text from a Reader (such as file)
 	scanner := bufio.NewScanner(r)
 
-	// If the count lines flag is not set, we want to count words so we define
+	// If the count lines or count bytes flag is not set, we want to count words so we define
 	// the scanner split type to words (default is split by lines)
-	if !countLines {
+	switch {
+	case countBytes:
+		scanner.Split(bufio.ScanBytes)
+	case countLines:
+		scanner.Split(bufio.ScanLines)
+	default:
 		scanner.Split(bufio.ScanWords)
 	}
 
@@ -37,8 +42,10 @@ func count(r io.Reader, countLines bool) int {
 func main() {
 	// Defining a boolean flag -l to count lines instead of words
 	lines := flag.Bool("l", false, "Count lines")
+	// Defining a boolean flag -b to count bytes
+	bytes := flag.Bool("b", false, "Count bytes")
 	flag.Parse()
 	// Calling the count function to count the number of words (or lines)
 	// received from the Standard Input
-	fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *lines, *bytes))
 }
