bbdf67a76f49a249db00a7451836546acbbd085b
Manuel <manuelinfosec@gmail.com>
2024-01-07 23:01:03 +0100

    test(wc): counting by bytes

--- wc/main_test.go
+++ wc/main_test.go
@@ -9,7 +9,7 @@ import (
 func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3 word4\n")
 	expected_count := 4
-	result := count(b, false)
+	result := count(b, false, false)
 
 	if result != expected_count {
 		t.Errorf("Expected %d, got %d instead.\n", expected_count, result)
@@ -19,10 +19,20 @@ func TestCountWords(t *testing.T) {
 func TestCountLines(t *testing.T) {
 	b := bytes.NewBufferString("word1\nword2\nword3\nword4")
 	expected_count := 4
-	result := count(b, true)
+	result := count(b, true, false)
 
 	if expected_count != result {
 		t.Errorf("Expected %d, got %d instead", expected_count, result)
 	}
 
+}
+
+func TestCountBytes(t *testing.T) {
+	b := bytes.NewBufferString("word1 word2 word3 word4 word5")
+	expected_count := 29
+	result := count(b, false, true)
+
+	if expected_count != result {
+		t.Errorf("Expected %d, got %d instead", expected_count, result)
+	}
 }
