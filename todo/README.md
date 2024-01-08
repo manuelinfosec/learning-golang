235028fee70d769c48d0835c789a53a8608ae70c
Manuel <manuelinfosec@gmail.com>
2024-01-08 01:14:09 +0100

    todo: Defining `items` & `List` APIs

---
+++ todo/README.md
@@ -0,0 +1,5 @@
+## Defining To-Do API
+
+- item: a single to-do item implemented using a Go struct.
+
+- List: represents a list of to-do items implemented by a slice of instances of the `item` type.
---
+++ todo/todo.go
@@ -0,0 +1,60 @@
+package todo
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+// item struct represents a ToDo item
+type item struct {
+	Task        string
+	Done        bool
+	CreatedAt   time.Time
+	CompletedAt time.Time
+}
+
+// List represents a list of ToDo items
+type List []item
+
+func (l *List) Add(task string) {
+	t := item{
+		Task:        task,
+		Done:        false,
+		CreatedAt:   time.Now(),
+		CompletedAt: time.Time{},
+	}
+	*l = append(*l, t)
+}
+
+// Complete method marks a ToDo item as completed by
+// setting Done = true and CompletedAt to the current time
+func (l *List) Complete(i int) error {
+	ls := *l
+	if i <= 0 || i > len(ls) {
+		return fmt.Errorf("Item %d does not exist", i)
+	}
+	// Adjusting index for 0-based index
+	ls[i-1].Done = true
+	ls[i-1].CompletedAt = time.Now()
+
+	return nil
+}
+
+// Delete method deletes a ToDo item from the list
+func (l *List) Delete(i int) error {
+	ls := *l
+	if i <= 0 || i > len(ls) {
+		return fmt.Errorf("Item %d does not exist", i)
+	}
+
+	// Adjusting index for 0-based index
+	// Appending all elements >i to all elements less than i using
+	// the spread operator
+	*l = append(ls[:i-1], ls[i:]...)
+
+	return nil
+}
