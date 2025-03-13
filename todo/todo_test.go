83620f56e23932f1aefada375eb409a4dc3500f7
Manuel <manuelinfosec@gmail.com>
2025-03-13 05:49:50 +0100

    Tested more List methods

--- todo/todo_test.go
+++ todo/todo_test.go
@@ -1,7 +1,8 @@
 package todo_test
 
 import (
-	"fmt"
+	"ioutil"
+	"os"
 	"testing"
 
 	"example.com/todo"
@@ -44,5 +45,61 @@ func TestComplete(t *testing.T) {
 	// Test if the task was successfully completed
 	if !l[0].Done {
 		t.Errorf("New Task should be completed.")
+	}
+}
+
+func TestDelete(t *testing.T) {
+	l := todo.List{}
+
+	tasks := []string{
+		"New Task 1",
+		"New Task 2",
+		"New Task 3",
+	}
+
+	for _, task := range tasks {
+		l.Add(task)
+	}
+
+	if l[0].Task != tasks[0] {
+		t.Errorf("Expected %q, got %q", l[0].Task, tasks[0])
+	}
+
+	l.Delete(2)
+
+	if len(l) != 2 {
+		t.Errorf("Expected %q, got %q", 2, len(l))
+	}
+}
+
+func TestSaveGet(t *testing.T) {
+	l1 := todo.List{}
+	l2 := todo.List{}
+
+	taskName := "New Task"
+	l1.Add(taskName)
+
+	if l1[0].Task != taskName {
+		t.Errorf("Expected %q, got %q", taskName, l1[0].Task)
+	}
+
+	tf, err := ioutil.TempFile("", "")
+	if err != nil {
+		t.Fatalf("Erroor creating temp file: %s", err)
+
+	}
+
+	defer os.Remove(tf.Name())
+
+	if err := l1.Save(tf.Name()); err != nil {
+		f.Fatalf("Error savinig list to file: %s", err)
+	}
+
+	if err := l2.Get(tf.Name()); err != nil {
+		t.Fatalf("Error getting list from file: %s", err)
+	}
+
+	if l1[0].Task != l2[0].Task {
+		t.Errorf("Task %q should match %q task", l2[0].Task, l1[0].Task)
 	}
 }
